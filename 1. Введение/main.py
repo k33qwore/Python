86091e96087db3b37a51e7cd965c5fe64fc5f516
Андрей Боев <a.boev.it@gmail.com>
2025-02-02 15:05:30 +0300

    Разбор темы 1. Введение

---
+++ 1. Введение/main.py
@@ -0,0 +1,25 @@
+"""
+print('Text 1', 'Text 2', 'Text 3', end=' ')
+print('Text 4', 'Text 5', 'Text 6')
+"""
+
+"""
+print('\'Text 1\'', '\'Text 2\'', '\'Text 3\'', sep='\n')
+"""
+
+"""
+name = input('Введите ваше имя: ')
+age = input('Введите ваш возраст: ')
+print(f'Меня зовут {name}, мне {age} лет')
+"""
+
+"""
+Плохой пример:
+print('Меня зовут ', name, ', мне ', age, ' лет', sep='')
+"""
+
+"""
+num1 = int(input())
+num2 = int(input())
+print(num1 * num2)
+"""
